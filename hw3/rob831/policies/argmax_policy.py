f6f16169ad9b406999414526181ee6545d88ac64
Shrinivas Ramasubramanian <shrinivr@gpu11.int.autonlab.org>
2025-03-13 15:22:48 -0400

    hw3 pre

--- hw3/rob831/policies/argmax_policy.py
+++ hw3/rob831/policies/argmax_policy.py
@@ -14,4 +14,6 @@ class ArgMaxPolicy(object):
         
         ## TODO return the action that maxinmizes the Q-value 
         # at the current observation as the output
+        qa_values = self.critic.qa_values(observation)
+        action = np.argmax(qa_values, axis=1)
         return action.squeeze()
